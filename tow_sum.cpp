efa9abb9af37dc47da03f7a238f53254a176444a
Ansh <anshgutpacode@gmail.com>
2026-06-30 23:49:59 +0530

    TRIAL

---
+++ tow_sum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp; // value -> index
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = twoSum(nums, target);
+
+    cout << "Indices: ";
+    for (int x : ans)
+        cout << x << " ";
+
+    return 0;
+}
